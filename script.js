79390d90b7fc474733e0d00b7b8d88e5999d1357
cappeloow <cappe1996@hotmail.com>
2023-02-16 17:35:28 +0100

    some of the funcionallity work now, atleast I'm getting one teeth to fall down

--- script.js
+++ script.js
@@ -12,11 +12,39 @@ var thirdUpperTooth = document.querySelector(".toothUpThree");
 var fourUpperTooth = document.querySelector(".toothUpFour");
 var fiveUpperTooth = document.querySelector(".toothUpFive");
 var topTeeths = [firstUpperTooth, secUpperTooth, thirdUpperTooth, fourUpperTooth, fiveUpperTooth];
-console.log(topTeeths);
 var firstBottomTooth = document.querySelector(".toothDownOne");
 var secondBottomTooth = document.querySelector(".toothDownTwo");
 var thirdBottomTooth = document.querySelector(".toothDownThree");
 var fourBottomTooth = document.querySelector(".toothDownFour");
 var fiveBottomTooth = document.querySelector(".toothDownFive");
 var bottomTeeths = [firstBottomTooth, secondBottomTooth, thirdBottomTooth, fourBottomTooth, fiveBottomTooth];
-console.log(bottomTeeths);
+var arrayOfStrings = ["Are you sure?", "You'll regret your actions!", "DON'T!!", "Soon you'll be Tomato paste", "What if... you die?", "This one is safe", "Don't worry my friend", "Time is ticking.."];
+var randomNr = Math.floor(Math.random() * 5);
+(function () {
+    var _a, _b;
+    var deadlyTooth = bottomTeeths.filter(function (tooth, index) { return index === randomNr; });
+    var firstTooth = (_a = deadlyTooth === null || deadlyTooth === void 0 ? void 0 : deadlyTooth[0]) !== null && _a !== void 0 ? _a : null; // Destructure the first element from the array
+    var upperTeethDeadly = topTeeths.filter(function (tooth, index) { return index === randomNr; });
+    var UpperTeeth = (_b = upperTeethDeadly === null || upperTeethDeadly === void 0 ? void 0 : upperTeethDeadly[0]) !== null && _b !== void 0 ? _b : null;
+    if (firstTooth) {
+        firstTooth.id = "deadly";
+    }
+    bottomTeeths.forEach(function (tooth) {
+        tooth === null || tooth === void 0 ? void 0 : tooth.addEventListener("click", function () {
+            console.log(tooth);
+            if (tooth.id === "deadly" && UpperTeeth !== null) {
+                UpperTeeth.id = "down";
+                console.log(UpperTeeth);
+            }
+            else {
+                console.log("Bad");
+            }
+        });
+    });
+})();
+// behöver ett ID som är beforedeath alltså sen death ontrigger? alltså att tanden går ner, och detta ID 
+//ska random ges till någon av de tänderna som är uppe, detta id aktiveras 
+// när tanden under har tryckts på
+// de andra tänderna där uppe ska bara stanna kvar, medans bottom teeths ska sjunka lite. 
+//vill vi ha en counter av hur många gånger man har lyckats klara sig från bettet?
+// kanske lite text som gör en osäker på sina val, timer?
--- script.ts
+++ script.ts
@@ -17,7 +17,7 @@ let thirdUpperTooth = document.querySelector(".toothUpThree")
 let fourUpperTooth = document.querySelector(".toothUpFour")
 let fiveUpperTooth = document.querySelector(".toothUpFive")
 let topTeeths = [firstUpperTooth, secUpperTooth, thirdUpperTooth, fourUpperTooth,fiveUpperTooth]
-console.log(topTeeths)
+
 
 let firstBottomTooth = document.querySelector(".toothDownOne")  
 let secondBottomTooth = document.querySelector(".toothDownTwo")  
@@ -26,12 +26,47 @@ let fourBottomTooth = document.querySelector(".toothDownFour")
 let fiveBottomTooth = document.querySelector(".toothDownFive")  
 
 let bottomTeeths = [firstBottomTooth, secondBottomTooth, thirdBottomTooth, fourBottomTooth,fiveBottomTooth]
-console.log(bottomTeeths)
 
-// behöver ett ID som är death? alltså att tanden går ner, och detta ID 
+let arrayOfStrings:string[] = ["Are you sure?", "You'll regret your actions!", "DON'T!!", "Soon you'll be Tomato paste", "What if... you die?", "This one is safe", "Don't worry my friend", "Time is ticking.." ]
+let randomNr:number = Math.floor(Math.random()*5); 
+
+
+(() => {
+    const deadlyTooth = bottomTeeths.filter((tooth, index) => index === randomNr);
+    const firstTooth = deadlyTooth?.[0] ?? null; // Destructure the first element from the array
+    const upperTeethDeadly = topTeeths.filter((tooth,index) => index===randomNr) 
+    const UpperTeeth = upperTeethDeadly?.[0] ?? null
+
+
+    if (firstTooth) {
+        firstTooth.id = "deadly"; 
+    }
+
+    bottomTeeths.forEach(tooth => {
+        tooth?.addEventListener("click", () => {
+            console.log(tooth);
+            if (tooth.id === "deadly" && UpperTeeth !== null) {
+                UpperTeeth.id = "down";
+                console.log(UpperTeeth)
+            } else {
+                console.log("Bad");
+            }
+        });
+    })
+    
+})();
+
+
+
+
+
+
+// behöver ett ID som är beforedeath alltså sen death ontrigger? alltså att tanden går ner, och detta ID 
 //ska random ges till någon av de tänderna som är uppe, detta id aktiveras 
 // när tanden under har tryckts på
 // de andra tänderna där uppe ska bara stanna kvar, medans bottom teeths ska sjunka lite. 
 
 
+//vill vi ha en counter av hur många gånger man har lyckats klara sig från bettet?
+// kanske lite text som gör en osäker på sina val, timer?
 
