b5b726d5bc4e00762c43226d72d3b673ea652f5a
cappeloow <cappe1996@hotmail.com>
2023-02-16 12:44:59 +0100

    Now the teeth's are in place, need to fix the functionallities

---
+++ script.js
@@ -0,0 +1,22 @@
+// const myArray:number[] = [1,2,3,4,5];
+// const totalSumWithMathpow = (myArray: number[]):number => {
+//     let arrayOfPow:number[] = myArray.map((number: number) => Math.pow(number,2));
+//     let totalSum:number = arrayOfPow.reduce((prev,next) => prev + next, 0)
+//     return totalSum;
+// }
+// console.log(totalSumWithMathpow(myArray))
+// console.log(myArray);
+var firstUpperTooth = document.querySelector(".toothUpOne");
+var secUpperTooth = document.querySelector(".toothUpTwo");
+var thirdUpperTooth = document.querySelector(".toothUpThree");
+var fourUpperTooth = document.querySelector(".toothUpFour");
+var fiveUpperTooth = document.querySelector(".toothUpFive");
+var topTeeths = [firstUpperTooth, secUpperTooth, thirdUpperTooth, fourUpperTooth, fiveUpperTooth];
+console.log(topTeeths);
+var firstBottomTooth = document.querySelector(".toothDownOne");
+var secondBottomTooth = document.querySelector(".toothDownTwo");
+var thirdBottomTooth = document.querySelector(".toothDownThree");
+var fourBottomTooth = document.querySelector(".toothDownFour");
+var fiveBottomTooth = document.querySelector(".toothDownFive");
+var bottomTeeths = [firstBottomTooth, secondBottomTooth, thirdBottomTooth, fourBottomTooth, fiveBottomTooth];
+console.log(bottomTeeths);
---
+++ script.ts
@@ -0,0 +1,31 @@
+// const myArray:number[] = [1,2,3,4,5];
+
+
+// const totalSumWithMathpow = (myArray: number[]):number => {
+//     let arrayOfPow:number[] = myArray.map((number: number) => Math.pow(number,2));
+//     let totalSum:number = arrayOfPow.reduce((prev,next) => prev + next, 0)
+//     return totalSum;
+// }
+
+// console.log(totalSumWithMathpow(myArray))
+// console.log(myArray);
+
+
+let firstUpperTooth = document.querySelector(".toothUpOne")
+let secUpperTooth = document.querySelector(".toothUpTwo")
+let thirdUpperTooth = document.querySelector(".toothUpThree")
+let fourUpperTooth = document.querySelector(".toothUpFour")
+let fiveUpperTooth = document.querySelector(".toothUpFive")
+let topTeeths = [firstUpperTooth, secUpperTooth, thirdUpperTooth, fourUpperTooth,fiveUpperTooth]
+console.log(topTeeths)
+
+let firstBottomTooth = document.querySelector(".toothDownOne")  
+let secondBottomTooth = document.querySelector(".toothDownTwo")  
+let thirdBottomTooth = document.querySelector(".toothDownThree")  
+let fourBottomTooth = document.querySelector(".toothDownFour")  
+let fiveBottomTooth = document.querySelector(".toothDownFive")  
+
+let bottomTeeths = [firstBottomTooth, secondBottomTooth, thirdBottomTooth, fourBottomTooth,fiveBottomTooth]
+console.log(bottomTeeths)
+
+
